3051c18a1072429e460ddc3abda8ff4f50743c77
Cay <412425870@qq.com>
2017-08-10 16:40:00 +0800

    更新代码

--- src/main/java/org/com/cay/service/impl/EmployeeServiceImpl.java
+++ src/main/java/org/com/cay/service/impl/EmployeeServiceImpl.java
@@ -2,23 +2,52 @@ package org.com.cay.service.impl;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.com.cay.dao.IEmployeeMapper;
 import org.com.cay.entity.Employee;
+import org.com.cay.redis.IJedisClient;
 import org.com.cay.service.IEmployeeService;
+import org.com.cay.utils.JsonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class EmployeeServiceImpl implements IEmployeeService {
 
 	@Autowired
 	private IEmployeeMapper employeeMapper;
-
+	
+	@Autowired
+	@Qualifier("jedisClientPool")
+	private IJedisClient jedisClient;
+	
 	@Override
 	public List<Employee> list() {
 		// TODO Auto-generated method stub
+		
+		//先查询缓存
+		//添加缓存不能影响正常业务逻辑
+		try{
+			String json = jedisClient.hget("emp", "list");
+			if(StringUtils.isNotBlank(json)){
+				List<Employee> employees = JsonUtils.jsonToList(json, Employee.class);
+				return employees;
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
 		List<Employee> list = employeeMapper.list();
-
+		
+		//把结果添加到缓存
+		try {
+			jedisClient.hset("emp", "list", JsonUtils.objectToJson(list));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		return list;
 	}
 
--- src/main/java/org/com/cay/service/impl/UserServiceImpl.java
+++ src/main/java/org/com/cay/service/impl/UserServiceImpl.java
@@ -2,10 +2,14 @@ package org.com.cay.service.impl;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.com.cay.dao.IUserMapper;
 import org.com.cay.entity.User;
+import org.com.cay.redis.IJedisClient;
 import org.com.cay.service.IUserService;
+import org.com.cay.utils.JsonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,26 +18,68 @@ public class UserServiceImpl implements IUserService {
 	@Autowired
 	private IUserMapper userMapper;
 
+	@Autowired
+	@Qualifier("jedisClientPool")
+	private IJedisClient jedisClient;
+
 	@Override
 	public void saveUser(User user) {
 		// TODO Auto-generated method stub
 		user.setPassword("111111");
 		userMapper.saveUser(user);
 
+		// 一旦新添加了数据，需要删除redis缓存中的数据，
+		try {
+			jedisClient.hdel("user", "list");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public List<User> list() {
 		// TODO Auto-generated method stub
+		try {
+			String json = jedisClient.hget("user", "list");
+			if (StringUtils.isNotBlank(json)) {
+				List<User> users = JsonUtils.jsonToList(json, User.class);
+				return users;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		List<User> list = userMapper.list();
+
+		try {
+			jedisClient.hset("user", "list", JsonUtils.objectToJson(list));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return list;
 	}
 
 	@Override
 	public User getUser(Integer id) {
 		// TODO Auto-generated method stub
+		try {
+			String json = jedisClient.hget("user", id + "");
+			if (StringUtils.isNotBlank(json)) {
+				User user = JsonUtils.jsonToPojo(json, User.class);
+				return user;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		User user = userMapper.getUser(id);
 
+		try {
+			jedisClient.hset("user", id + "", JsonUtils.objectToJson(user));
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return user;
 	}
 
